2b8925d386b4bee4c4cda28aecf270c2f87b8513
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-24 02:55:07 -0300

    Menu com rotas criado

--- site-grupo1/src/pages/Menu/Menu.js
+++ site-grupo1/src/pages/Menu/Menu.js
@@ -1,80 +1,112 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
   makeStyles,
+  AppBar,
+  Tabs,
+  Tab,
+  Typography,
+  Box,
 } from "@material-ui/core";
-import { Paper, Tabs, Tab } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(1),
-  },
-  title: {
-    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
 }));
 
 function Menu() {
   const classes = useStyles();
-  const history = useHistory();
-  const [value, setValue] = React.useState(2);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const history = useHistory();
+
   return (
     <div className={classes.root}>
-      
       <AppBar position="static">
-        <Toolbar>
-          <Button
-            onClick={() => {
-              history.push("Cadastro");
-            }}
-          >
-            Cadastro
-          </Button>
-          <Button
-            onClick={() => {
-              history.push("Historia");
-            }}
-          >
-            História
-          </Button>
-          <Button
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="menu-superior"
+          centered
+        >
+          <Tab
+            label="Home"
+            {...a11yProps(0)}
             onClick={() => {
               history.push("Home");
             }}
-          >
-            Home
-          </Button>
-          <Button
+          />
+          <Tab
+            label="Historia"
+            {...a11yProps(4)}
+            onClick={() => {
+              history.push("Historia");
+            }}
+          />
+          <Tab
+            label="Perfil"
+            {...a11yProps(2)}
             onClick={() => {
               history.push("Perfil");
             }}
-          >
-            Perfil
-          </Button>
-          <Typography className={classes.title} /> 
-          <Button
-            variant="contained"
-            color="secondary"
+          />
+          <Tab
+            label="Login"
+            {...a11yProps(3)}
             onClick={() => {
               history.push("Login");
             }}
-          >
-            Login
-          </Button>
-        </Toolbar>
+          />
+          <Tab
+            label="Cadastro"
+            {...a11yProps(1)}
+            onClick={() => {
+              history.push("Cadastro");
+            }}
+          />
+        </Tabs>
       </AppBar>
+      <TabPanel value={value} index={0} />
+      <TabPanel value={value} index={1} />
+      <TabPanel value={value} index={2} />
+      <TabPanel value={value} index={3} />
     </div>
   );
 }
