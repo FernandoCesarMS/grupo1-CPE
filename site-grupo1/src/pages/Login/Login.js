36b5daf78d9ec21982472e8f27457dc7d19ca404
Daniel Carvalho <daniel278p@gmail.com>
2020-10-24 19:34:31 -0300

    cadastro1

--- site-grupo1/src/pages/Login/Login.js
+++ site-grupo1/src/pages/Login/Login.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 
 function Login() {
+  
   return (
     <div>
       <center>
         <h1>Login</h1>
       </center>
+      
     </div>
   );
 }
