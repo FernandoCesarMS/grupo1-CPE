172683e999d47571bf30300d488d9ba2a0e1f0d8
Raphael Leivas <raphaelhenrique2013@outlook.com>
2020-10-25 18:27:52 -0300

    Paginas de login e cadastro atualizadas e estilizadas

--- site-grupo1/src/pages/Cadastro/Cadastro.js
+++ site-grupo1/src/pages/Cadastro/Cadastro.js
@@ -1,13 +1,89 @@
 import React from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import "./Cadastro.css";
 
 function Cadastro() {
+  const [senha1, setSenha1] = useState(0);
+  const [senha2, setSenha2] = useState(1);
+  const history = useHistory();
+
   return (
-    <div>
-      <center>
-        <h1>Cadastro</h1>
-      </center>
+    <div className="baseCAD">
+      <div className="corpoCAD">
+        <center>
+          <h1>Cadastro</h1>
+        </center>
+
+        <Form>
+          <Form.Group controlId="Nome">
+            <Form.Label>Nome Completo</Form.Label>
+            <Form.Control placeholder="Nome completo" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="Idade">
+              <Form.Label>Idade</Form.Label>
+              <Form.Control placeholder="Idade" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="CPF">
+              <Form.Label>CPF</Form.Label>
+              <Form.Control placeholder="CPF" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group controlId="Username">
+            <Form.Label>Nome de usuário</Form.Label>
+            <Form.Control placeholder="Nome de usuário" />
+          </Form.Group>
+
+          <Form.Group controlId="Email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control placeholder="Endereço de email" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="Senha">
+              <Form.Label>Crie sua senha</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Digite sua senha"
+                onChange={(event) => {
+                  setSenha1(event.target.value);
+                }} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="SenhaConf">
+              <Form.Label>Confirme sua senha</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Digite sua senha"
+                onChange={(event) => {
+                  setSenha2(event.target.value);
+                }} />
+            </Form.Group>
+          </Form.Row>
+
+          <Button
+            variant="primary"
+            type="Cadastrar"
+            onClick={() => {
+              if (senha1 !== senha2)
+                alert("Senhas diferentes!");
+              else {
+                alert("Cadastrado!");
+                history.push("/Login");
+              }
+            }}>
+            Cadastrar
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
 
 export default Cadastro;
+
--- site-grupo1/src/pages/Login/Login.js
+++ site-grupo1/src/pages/Login/Login.js
@@ -1,15 +1,45 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-
+import { useHistory, Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 function Login() {
-  
+  const history = useHistory();
+
   return (
-    <div>
-      <center>
+    <div className="baseLOG">
+      <div className="corpoLOG">
         <h1>Login</h1>
-      </center>
-      
+        <Form>
+          <Form.Group controlId="username">
+            <Form.Label>Nome de usuário</Form.Label>
+            <Form.Control type="username" placeholder="Nome de usuário" />
+          </Form.Group>
+
+          <Form.Group controlId="senha">
+            <Form.Label>Senha</Form.Label>
+            <Form.Control type="password" placeholder="Senha" />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="login"
+            onClick={() => {
+              history.push("/Perfil");
+            }}>
+            LOGIN
+          </Button>
+
+          <br/>
+          <input id="logar" type="checkbox"/>
+          <label htmlFor="logar">Mantenha-me logado</label>
+
+          <center>
+            <p>Ainda não tem uma conta? </p>
+            <Link to="/Cadastro">Criar uma nova conta</Link>
+          </center>
+        </Form>
+      </div>
     </div>
   );
 }
