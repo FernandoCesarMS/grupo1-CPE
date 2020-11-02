d7a1c4405f0ada63548436e4e84bf9b0a5c55088
Fernando Cesar <cmnando.silva@gmail.com>
2020-11-02 02:15:15 -0300

    Adicionando avatar

--- site-grupo1/src/pages/Perfil/Perfil.js
+++ site-grupo1/src/pages/Perfil/Perfil.js
@@ -1,19 +1,45 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 import "./Perfil.css";
 /*
 A página Perfil tem como função possibilitar ao usuário a visualização dos
 seus principais dados.
+Para fazer a imagem principal do perfil, foi utilizado o componente Avatar
+do framework material
 */
+const useStyles = makeStyles((theme) => ({
+  imagemPrincipal: { // Defina como será a imagem
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(5),  // Afasta os outros componentes
+      width: theme.spacing(30),  // Largura da imagem
+      height: theme.spacing(30), // Altura da imagem
+    },
+  },
+}));
+
 function Perfil() {
+
+  const classes = useStyles();
+
   return (
-    <div className="basePERFIL">
-      <center>
+  <div className="basePERFIL"> {/* Div que armaneza a página inteira */}
+
+      <div className="title"> {/* Div que contêm o titulo */}
         <h1>Perfil</h1>
-      </center>
-      <p>Nome: </p>
-      <p>Gênero: </p>
-      <p>Clã: </p>
-      <p>Classe: </p>
+      </div>
+
+      <div className="body"> {/* Div que contêm as informações do perfil */}
+        <div className={classes.imagemPrincipal}> {/* Div que contêm a imagem do personagem */}
+          <Avatar src="/broken-image.jpg" />
+        </div>
+        <p>Nome: </p>
+        <p>Gênero: </p>
+        <p>Clã: </p>
+        <p>Técnica: </p>
+      </div>
+
     </div>
   );
 }
