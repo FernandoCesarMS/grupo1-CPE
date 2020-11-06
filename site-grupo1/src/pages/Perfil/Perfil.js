acaac74ba8002b2c680115d3aad59521d20eaca6
Fernando Cesar <cmnando.silva@gmail.com>
2020-11-05 22:59:26 -0300

    Alteração no CSS da página

--- site-grupo1/src/pages/Perfil/Perfil.js
+++ site-grupo1/src/pages/Perfil/Perfil.js
@@ -9,33 +9,39 @@ Para fazer a imagem principal do perfil, foi utilizado o componente Avatar
 do framework material
 */
 const useStyles = makeStyles((theme) => ({
-  imagemPrincipal: { // Defina como será a imagem
+  imagemPrincipal: {
+    // Defina como será a imagem
     display: "flex",
     "& > *": {
-      margin: theme.spacing(5),  // Afasta os outros componentes
-      width: theme.spacing(30),  // Largura da imagem
+      margin: theme.spacing(5), // Afasta os outros componentes
+      width: theme.spacing(30), // Largura da imagem
       height: theme.spacing(30), // Altura da imagem
     },
   },
 }));
 
 function Perfil() {
-
   const classes = useStyles();
 
   return (
-  <div className="basePERFIL"> {/* Div que armazena a página inteira */}
-
-      <div className="body"> {/* Div que contêm as informações do perfil */}
-        <div className={classes.imagemPrincipal}> {/* Div que contêm a imagem do personagem */}
+    <div className="basePERFIL">
+      {" "}
+      {/* Div que armazena a página inteira */}
+      <div className="body">
+        {" "}
+        {/* Div que contêm as informações do perfil */}
+        <div className={classes.imagemPrincipal}>
+          {" "}
+          {/* Div que contêm a imagem do personagem */}
           <Avatar src="/broken-image.jpg" />
         </div>
-        <p>Nome: </p>
-        <p>Gênero: </p>
-        <p>Clã: </p>
-        <p>Técnica: </p>
+        <div className="text">
+          <p>Nome: </p>
+          <p>Gênero: </p>
+          <p>Clã: </p>
+          <p>Técnica: </p>
+        </div>
       </div>
-
     </div>
   );
 }
